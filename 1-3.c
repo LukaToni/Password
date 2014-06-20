b19486401046f0536d339f429b2db1b1d3e3c735
Luka <luka.toni93@gmail.com>
2014-06-20 17:28:24 +0200

    Update 1-3.c

--- 1-3.c
+++ 1-3.c
@@ -1,1 +1,37 @@
 #include <stdio.h>
+
+int main()
+{
+  char* chars = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+  char* nums = {'0','1','2','3','4','5','6','7','8','9'};
+  
+  char* password = malloc(sizeof(char)*25);
+  
+  int i;
+  
+  int c = 4;
+  int n = 4;
+  
+  for(i = 8; i <= 20; i = i + 2)
+  {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    printf("%d\n", password);
+    
+    c++;
+    n++;
+  }
+ return 0; 
+}
